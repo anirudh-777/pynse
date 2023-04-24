abdbca5d5ee99600a681e16fb786e7c753c234d8
Anirudh Prakash <anirudh@pulsops.com>
2023-04-24 17:37:59 +0530

    Adds test for get_stock_symbols

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,4 +1,4 @@
-from pynse.pynse import get_stock_data
+from pynse.pynse import get_stock_data, get_stock_symbols
 
 
 def test_get_stock_data():
@@ -9,3 +9,12 @@ def test_get_stock_data():
     assert stock_data.shape[1] == 14
     assert stock_data.index.name == "symbol"
     assert stock_data.index.tolist().sort() == stock_list.sort()
+
+
+def test_get_all_stock_symbols():
+    symbols = get_stock_symbols()
+    assert symbols is not None
+    assert len(symbols) > 0
+    assert "RELIANCE" in symbols
+    assert "ITC" in symbols
+    assert "SBIN" in symbols
